5c8ba2d1f7bbe4d3aa4556d4156d08736c5b12c4
kleber-s-araujo <124310150+kleber-s-araujo@users.noreply.github.com>
2024-11-29 14:12:49 -0300

    Adding new exercises for classes e herença

---
+++ ProgramacaoOrientadaAObjetos/04/consumidor.mjs
@@ -0,0 +1,26 @@
+import {Carro} from "./carro.mjs"
+
+var carro = new Carro(55)
+console.log(carro.capacidade)
+console.log(carro.tanque)
+
+carro.tanque = 10
+console.log(carro.tanque)
+
+carro.tanque = 10
+console.log(carro.tanque)
+
+carro.tanque = 10
+console.log(carro.tanque)
+
+carro.tanque = 10
+console.log(carro.tanque)
+
+carro.tanque = 10
+console.log(carro.tanque)
+
+carro.tanque = 10
+console.log(carro.tanque)
+
+carro.tanque = 10
+console.log(carro.tanque)
---
+++ ProgramacaoOrientadaAObjetos/05/Consumidor.mjs
@@ -0,0 +1,17 @@
+import { CarroComPlaca } from "./CarroComPlaca.mjs"
+import { Locadora } from "./Locadora.mjs"
+
+var locadora = new Locadora()
+locadora.adicionaCarro(new CarroComPlaca("ABC-9I23"))
+locadora.adicionaCarro(new CarroComPlaca("DEF-0U74"))
+
+locadora.consultaCarros()
+
+locadora.abasteceCarro(0, 30)
+locadora.consultaCarros()
+
+locadora.abasteceCarro(1, 30)
+locadora.consultaCarros()
+
+locadora.abasteceCarro(0, 30)
+locadora.consultaCarros()
